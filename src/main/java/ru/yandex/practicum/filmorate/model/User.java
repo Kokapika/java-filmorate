8a242862d55ef8a1c982094ce67f6f302b496dd8
Kokapika <122634408+Kokapika@users.noreply.github.com>
2023-11-14 00:54:55 +0400

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private int id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+
+    public User(String login, String name, String email, LocalDate birthday) {
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
+}
