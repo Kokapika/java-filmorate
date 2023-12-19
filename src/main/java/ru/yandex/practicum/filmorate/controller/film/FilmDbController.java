454dc09799d4d5b5a5dac5ab5cc00a1375fd24cb
Kokapika <122634408+Kokapika@users.noreply.github.com>
2023-12-19 15:37:55 +0700

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/film/FilmDbController.java
@@ -0,0 +1,46 @@
+package ru.yandex.practicum.filmorate.controller.film;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.film.FilmDbService;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/films")
+public class FilmDbController {
+    private final FilmDbService filmDbService;
+
+    @GetMapping
+    public List<Film> getFilms() {
+        return filmDbService.getFilms();
+    }
+
+    @GetMapping("/{id}")
+    public Film getFilmById(@PathVariable Integer id) {
+        return filmDbService.getFilmById(id);
+    }
+
+    @PostMapping
+    public Film addFilm(@Valid @RequestBody Film film) {
+        checkFilm(film);
+        return filmDbService.addFilm(film);
+    }
+
+    @PutMapping
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        checkFilm(film);
+        return filmDbService.updateFilm(film);
+    }
+
+    private void checkFilm(Film film) {
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28)))
+            throw new ValidationException("Дата релиза не должна быть раньше 28 декабря 1895 года. Введено: "
+                    + film.getReleaseDate());
+    }
+}
