399e2e2f50845a49c7067c55c5fdb86e9ee05327
Oleg <emmoler@yandex.ru>
2023-12-28 15:53:25 +0300

    add-recommendations

--- src/main/java/ru/yandex/practicum/filmorate/service/LikeDbService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/LikeDbService.java
@@ -3,10 +3,13 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.FilmLikes;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.like.LikeStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +30,9 @@ public class LikeDbService {
         User user = userStorage.getUserById(userId);
         likesStorage.deleteLike(filmId, userId);
         return film;
+    }
+
+    public List<FilmLikes> getAllLikes() {
+        return likesStorage.getAllLikes();
     }
 }
