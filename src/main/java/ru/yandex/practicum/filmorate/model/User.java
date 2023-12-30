a26029ea89cedaf2bc9dd025e025115ccc329130
Arsen <laipan09@mail.ru>
2023-12-30 04:47:02 +0300

    add tests

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
     private Integer id;
     @Email
@@ -32,4 +34,3 @@ public class User {
         this.birthday = birthday;
     }
 }
-
